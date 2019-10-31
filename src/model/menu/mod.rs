2c2243c805cb540b4ae6483fb7b81b245c87ed10
Cristian Raña <cristian.rana@despegar.com>
2019-10-30 21:24:06 -0300

    Settings menu added

--- src/model/menu/mod.rs
+++ src/model/menu/mod.rs
@@ -5,11 +5,11 @@ use std::io::Write;
 use std::num::ParseIntError;
 
 pub fn run() {
-    display();
     let mut exit_option: bool = false;
 
     while !exit_option {
-        let mut option: Result<u32, ParseIntError> = utils::read_integer();
+        display_main_menu();
+        let option: Result<u32, ParseIntError> = utils::read_integer();
         match option {
             Ok(value) => {
                 match value {
@@ -18,11 +18,9 @@ pub fn run() {
                     }
                     1 => {
                         println!("Running system.");
-                        println!();
                     },
                     2 => {
-                        println!("Settings menu.");
-                        println!();
+                        settings_menu();
                     },
                     _ => {
                         print!("Wrong option! Retry: ");
@@ -38,11 +36,53 @@ pub fn run() {
     }
 }
 
-pub fn display() {
+pub fn display_main_menu() {
+    println!();
     println!("Gold Miner Menu!");
     println!("Select your option:");
     println!("[1] Run system");
     println!("[2] Settings");
     print!("Selection: [0 for quit] ");
+    io::stdout().flush();
+}
+
+fn settings_menu() {
+    let mut exit_option: bool = false;
+
+    while !exit_option {
+        display_settings_menu();
+        let option: Result<u32, ParseIntError> = utils::read_integer();
+        match option {
+            Ok(value) => {
+                match value {
+                    0 => {
+                        exit_option = true;
+                    }
+                    1 => {
+                        println!("Modifying miners.");
+                    },
+                    2 => {
+                        println!("Modifying zones.");
+                    },
+                    _ => {
+                        print!("Wrong option! Retry: ");
+                        io::stdout().flush();
+                    }
+                }
+            },
+            Err(..) => {
+                print!("Wrong option! Retry: ");
+                io::stdout().flush();
+            }
+        }
+    }
+}
+
+fn display_settings_menu() {
+    println!();
+    println!("Current settings:");
+    println!("[1] Miners: 0");
+    println!("[2] Zones: 0");
+    print!("What do you want to change? [0 for quit] ");
     io::stdout().flush();
 }
